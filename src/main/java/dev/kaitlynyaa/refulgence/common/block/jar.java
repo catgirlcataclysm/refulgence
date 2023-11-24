2f787c87e18f0d6fa1ff6d02b5a9cda9add21ddd
kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2023-11-24 17:06:00 -0500

    added redstone jar

--- src/main/java/dev/kaitlynyaa/refulgence/common/block/jar.java
+++ src/main/java/dev/kaitlynyaa/refulgence/common/block/jar.java
@@ -9,16 +9,13 @@ import net.minecraft.world.BlockView;
 
 public class jar extends Block {
     private static final VoxelShape JAR_SHAPE = Block.createCuboidShape(4, 0, 4, 12, 13, 12);
-	
     public jar(Settings settings) {
 		super(settings);
 	}
-	
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
 		return JAR_SHAPE;
 	}
-	
     @Override
 	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
 		return JAR_SHAPE;
